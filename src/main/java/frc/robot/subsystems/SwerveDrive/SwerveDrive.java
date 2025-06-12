992485ef7565c3f40c10b367ed6f4369ed57f0e8
XLFrie <xlfrys@gmail.com>
2025-06-11 20:00:42 -0700

    Add explanation to swerve drive calculations

--- src/main/java/frc/robot/subsystems/SwerveDrive/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive/SwerveDrive.java
@@ -39,15 +39,17 @@ public class SwerveDrive extends SubsystemBase {
   private final ModuleIO frontRight;
   private final ModuleIO backLeft;
   private final ModuleIO backRight;
-
-  private double headingOffset;
-
   private final StructPublisher<Pose2d> posePublisher =
       NetworkTableInstance.getDefault().getStructTopic("Pose", Pose2d.struct).publish();
   private final StructArrayPublisher<SwerveModuleState> currentModuleStatesPublisher =
       NetworkTableInstance.getDefault()
           .getStructArrayTopic("Current Module States", SwerveModuleState.struct).publish();
 
+  // This is used to calculate what the heading is relative to the driver station, changes based
+  // on alliance
+  private double headingOffset;
+
+  // Real pose in simulation (if not real)
   private StructPublisher<Pose2d> realPosePublisher;
 
   public SwerveDrive(GyroIO gyro, ModuleIO fl, ModuleIO fr, ModuleIO bl, ModuleIO br) {
@@ -74,16 +76,18 @@ public class SwerveDrive extends SubsystemBase {
       throw new RuntimeException("Failed to get PathPlanner config from GUI");
     }
 
-    //      TODO move pid constants to constants file (pls help i dont wanna do this)
+    // TODO move pid constants to constants file (pls help i dont wanna do this)
     AutoBuilder.configure(this::getPose, this::setPose, this::getChassisSpeed,
         (ChassisSpeeds speeds) -> this.drive(speeds, false),
         new PPHolonomicDriveController(new PIDConstants(10, 2, 0), new PIDConstants(4, 8, 0.3)),
-        ppConfig,
-        () -> {
+        ppConfig, () -> {
           Optional<DriverStation.Alliance> allianceOptional = DriverStation.getAlliance();
 
+          // Flip position to red if on red alliance
           if (allianceOptional.isPresent() && allianceOptional.get()
               .equals(DriverStation.Alliance.Red)) {
+            // Origin is set at blue alliance, if we're on red then we need to flip
+            // the heading
             headingOffset = Math.PI;
             return true;
           }
@@ -97,7 +101,7 @@ public class SwerveDrive extends SubsystemBase {
   }
 
   public void drive(ChassisSpeeds chassisSpeeds, boolean absolute) {
-    //    TODO make use swerve kinematics class
+    // TODO make use swerve kinematics class
     double heading = getHeading().getRadians() + headingOffset;
     calculateState(chassisSpeeds, heading, frontRight, absolute);
     calculateState(chassisSpeeds, heading, frontLeft, absolute);
@@ -112,6 +116,7 @@ public class SwerveDrive extends SubsystemBase {
 
     publishTelemetry();
 
+    // Some PIDs need to be calculated, do that here
     frontRight.tickPID();
     frontLeft.tickPID();
     backRight.tickPID();
@@ -122,7 +127,7 @@ public class SwerveDrive extends SubsystemBase {
     posePublisher.set(swerveDrivePoseEstimator.getEstimatedPosition());
     currentModuleStatesPublisher.set(getModuleStates());
 
-    //    TODO move this into robot container
+    // TODO move this into robot container
     if (realPosePublisher != null && RobotContainer.swerveDriveSimulation != null) {
       realPosePublisher.set(RobotContainer.swerveDriveSimulation.getSimulatedDriveTrainPose());
     }
@@ -130,24 +135,48 @@ public class SwerveDrive extends SubsystemBase {
 
   private void calculateState(ChassisSpeeds chassisSpeeds, double heading, ModuleIO module,
       boolean absolute) {
+    // VY is the desired left velocity, vx is the desired forward velocity
     Vector2 translationVector =
         new Vector2(-chassisSpeeds.vyMetersPerSecond, chassisSpeeds.vxMetersPerSecond);
+
+    // Accounts for heading in absolute movement, this is all the absolute flag does
     if (absolute)
       translationVector.rotate(-heading);
+
+    /*
+      Swerve drive kinematics are fairly simple.
+      There are two components to each module's desired state.
+        1. The translation vector - this is simply the desired translation velocity
+        2. The rotation vector - this is the desired angular velocity times the rotation unit
+        vector (which should be tangent to a line drawn from the module to the center of the
+        drivetrain)
+
+      The desired state is the sum of the two.
+     */
+
+
+    // Calculates rotation vector as described
     Vector2 rotationVector =
         module.getNormalRotationVec().copy().multiply(chassisSpeeds.omegaRadiansPerSecond);
 
+    // This will be the desired state
     translationVector.add(rotationVector);
 
+    // Gets the angle of the vector, 90 degrees is subtracted from the calculated angle because 
+    // heading angle's zero is set 90 degrees counterclockwise
     Angle vecAngle = Radians.of(translationVector.getDirection() - Math.PI / 2);
     double vecMagnitude = translationVector.getMagnitude();
 
+    // Current angle that wheel is facing.
     Angle steeringAngle = module.getSteerAngle().getMeasure();
 
+    // Calculates differences between opposite angle and angle calculated earlier
     Angle originalDifference = MathUtils.subtractAngles(vecAngle, steeringAngle);
     Angle oppositeDifference =
         MathUtils.subtractAngles(vecAngle.copy().plus(Radians.of(Math.PI)), steeringAngle);
 
+    // Determines if it's shorter to turn the opposite and run the motor backwards, or the 
+    // original calculated angle
     if (Math.abs(originalDifference.magnitude()) > Math.abs(oppositeDifference.magnitude())) {
       vecMagnitude *= -1;
       vecAngle = vecAngle.plus(Radians.of(Math.PI));
